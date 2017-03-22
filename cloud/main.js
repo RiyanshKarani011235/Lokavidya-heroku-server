db05b886ac4590e57ecc637cbaaf4e8a449e83ac
RiyanshKarani011235 <riyansh.karani.011235@gmail.com>
2017-03-23 04:10:09 +0530

    _

--- cloud/main.js
+++ cloud/main.js
@@ -1,1 +1,5 @@
 require('./api/SendOtp.js');
+require('./api/Stitch.js');
+
+// Installing ffmpeg on Heroku (https://github.com/h2non/heroku-buildpack-ffmpeg2)
+// fluent ffmpeg-API for node.js (https://www.npmjs.com/package/fluent-ffmpeg)
