cd3aafb412ed636f93c306900938fe00f32da266
RiyanshKarani011235 <riyansh.karani.011235@gmail.com>
2017-03-23 04:08:24 +0530

    ffmpeg installed

--- cloud/api/Stitch.js
+++ cloud/api/Stitch.js
@@ -1,1 +1,12 @@
+// FFMPEG installation for heroku
+// ------------------------------------------------------------------------------
+// heroku buildpacks:set https://github.com/shunjikonishi/heroku-buildpack-ffmpeg
+// git add .
+// git push heroku master
+// ------------------------------------------------------------------------------
+
 var parse = require('parse')
+
+var ffmpeg = require('fluent-ffmpeg');
+var command = ffmpeg();
+console.log(command)
