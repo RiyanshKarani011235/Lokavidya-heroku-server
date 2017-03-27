e32dea39f19f27ecc48386a7c2619041ef8dbfb4
RiyanshKarani011235 <riyansh.karani.011235@gmail.com>
2017-03-27 15:44:06 +0530

    api ready

--- cloud/api/Stitch.js
+++ cloud/api/Stitch.js
@@ -3,8 +3,26 @@
 // https://elements.heroku.com/buildpacks/issueapp/heroku-buildpack-ffmpeg
 // ------------------------------------------------------------------------------
 
+var path = require('path');
+var fs = require('fs');
 var parse = require('parse')
 var ffmpeg = require('fluent-ffmpeg');
+
+// variables
 var command = ffmpeg();
-console.log('hullalalalolelelo');
-console.log(command)
+
+Parse.cloud.define('stitch', (request, response) => {
+
+	var query = new Parse.Query("Project");
+	query.equalTo("objectId", request.params.project_id);
+	query.first({
+		success: function(object) {
+			console.log('project lookup succedeed : ' + object.id);
+			response.success('project lookup succedeed');
+		},
+		error: function() {
+			console.log("no project with id : " + request.params.project_id);
+			response.error("no project with id : " + request.params.project_id);
+		}
+	});
+});
