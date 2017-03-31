53b3b045ef3ac76b555fb03a84db4a7b066d4dd7
RiyanshKarani011235 <riyansh.karani.011235@gmail.com>
2017-03-31 13:59:36 +0530

    _

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+// https://elements.heroku.com/buildpacks/issueapp/heroku-buildpack-ffmpeg
+
 var fs = require('fs');
 var path = require('path');
 var ParseServer = require('parse-server').ParseServer;
 var express = require('express');
 
-var serverConfigDir = path.join(__dirname);
-var data = fs.readFileSync(path.join(serverConfigDir, 'config.json'));
+var configDir = path.join(__dirname, 'config');
+var data = fs.readFileSync(path.join(configDir, 'config.json'));
 var config;
 
 try {
@@ -17,7 +19,6 @@ try {
 // Heroku dynamically assigns your app a port, so you can't set the port
 // to a fixed number. Heroku adds the port to the env, so you can pull it from there.
 const PORT = process.env.PORT || 5000;
-// const DATABASE_URI = process.env.DATABASE_URI || process.env.MONGODB_URI;
 const DATABASE_URI = process.env.DATABASE_URI || process.env.MONGODB_URI || config.MONGO_DB_URL;
 const CLOUD_URL = process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js';
 const APP_ID = process.env.APP_ID || config.APP_ID;
@@ -26,17 +27,6 @@ const SERVER_URL = process.env.SERVER_URL || 'http://localhost:' + PORT + '/pars
 
 if (!DATABASE_URI) {
   throw new Error('DATABASE_URI not specified');
-}
-
-var printConfig = function() {
-	console.log('SERVER CONFIG');
-	console.log('-------------');
-	console.log('PORT : ' + PORT);
-	console.log('DATABASE_URI : ' + DATABASE_URI);
-	console.log('CLOUD_URL : ' + CLOUD_URL);
-	console.log('APP_ID : ' + APP_ID);
-	console.log('MASTER_KEY : ' + MASTER_KEY);
-	console.log('SERVER_URL : ' + SERVER_URL);
 }
 
 // generate parse-server middleware
@@ -71,6 +61,10 @@ app.use('/parse', api);
 // start http server
 app.listen(PORT, function() {
   console.log('Node app is running on port : ' + PORT);
+  console.log('Server Config : ');
+  console.log('----------------');
+  console.log('database URI : ' + DATABASE_URI);
+  console.log('app ID : ' + APP_ID);
+  console.log('master key : ' + MASTER_KEY);
+  console.log('server URL : ' + SERVER_URL);
 });
-
-printConfig();
