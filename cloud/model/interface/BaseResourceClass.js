6feac18134f7d0f006a9d94024f9da997841091f
RiyanshKarani011235 <riyansh.karani.011235@gmail.com>
2017-04-04 11:30:08 +0530

    _

--- cloud/model/interface/BaseResourceClass.js
+++ cloud/model/interface/BaseResourceClass.js
@@ -12,16 +12,32 @@ class BaseResourceClass extends BaseParseClass.BaseParseClass {
 var stitchSlide = (slide) => {
     console.log('stitchSlide : called');
     console.log(slide);
-    var c = slide.get('children_resources');
-    console.log(c);
-    c.fetch().then(
-        (object) => {
-            console.log('hurray');
+    var childrenResources = slide.get('children_resources');
+    console.log(childrenResources);
+    childrenResources.fetch().then(
+        () => {
+            console.log('successsssssssssss');
+            console.log(childrenResources);
+            var childResource = childrenResources[0];
+            childResource.fetch().then(
+                () => {
+                    if(childResource.className === 'Image') {
+                        // stitch image
+                        console.log('stitching Image slide');
+                    } else if(childResource.className == 'Video') {
+                        // stitch video
+                        console.log('stitching Video slide');
+                    } else if(childResource.className == 'Question') {
+                        // stitch question
+                        console.log('stitching Question slide');
+                    }
+                }
+            );
         },
         (error) => {
-            console.log('hmmm');
+            console.log(error);
         }
-    );
+    });
 }
 
 module.exports = {
