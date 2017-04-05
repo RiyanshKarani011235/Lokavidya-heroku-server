aed248f9f398ae30f7d36377a04bac167d2483f0
RiyanshKarani011235 <riyansh.karani.011235@gmail.com>
2017-04-06 03:39:28 +0530

    _

--- cloud/api/Stitch.js
+++ cloud/api/Stitch.js
@@ -35,25 +35,6 @@ var stitchProject = (projectObject) => {
 		success: (slides) => {
 			var elements = slides.get('elements');
 
-			// const exec = require('child_process').exec;
-			// const child = exec('ls',
-			// 	(error, stdout, stderr) => {
-			// 		console.log(`stdout: ${stdout}`);
-			// 		console.log(`stderr: ${stderr}`);
-			// 		if (error !== null) {
-			// 			console.log(`exec error: ${error}`);
-			// 		}
-			// });
-			//
-			// var reader = new FileReader();
-			// reader.onload = function () {
-			// 	console.log('reader.onload called');
-			// 	var file = new Parse.File("myfile.mp4", { base64: reader.result});
-			// 	console.log('before returning');
-			// 	return file;
-			// }
-			// reader.readAsDataURL(new File(outputFileName));
-
 			console.log(elements);
 			var stitchedFileNames = [];
 			var count = 0;
@@ -103,17 +84,35 @@ var stitchFinalVideo = (stitchedFileNames) => {
 	}
 	ffmpeg()
 		.input(stitchedFileNames[0])
-		
 		.inputOptions(inputOptionsArray)
 		.videoCodec('libx264')
 		.size('640x480')
-		.output('./outputFiles/finalvideo.mp4')
 		.output('./outputFiles/finalvideo.mp4')
 		.on('stderr', function(stderrLine) {
 			console.log('Stderr output: ' + stderrLine);
 		})
 		.on('end', function(stdout, stderr) {
 			console.log('Transcoding succeeded !');
+
+			// const exec = require('child_process').exec;
+			// const child = exec('ls',
+			// 	(error, stdout, stderr) => {
+			// 		console.log(`stdout: ${stdout}`);
+			// 		console.log(`stderr: ${stderr}`);
+			// 		if (error !== null) {
+			// 			console.log(`exec error: ${error}`);
+			// 		}
+			// });
+			//
+			var reader = new FileReader();
+			reader.onload = function () {
+				console.log('reader.onload called');
+				var file = new Parse.File("myfile.mp4", { base64: reader.result});
+				console.log('before returning');
+				return file;
+			}
+			reader.readAsDataURL(new File('./outputFiles/finalvideo.mp4'));
+
 			console.log('-------------------------- donezo');
 		})
 		.run();
