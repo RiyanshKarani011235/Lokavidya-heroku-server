01e3e8dd04f53c161b89275d54d0c741abe301f8
RiyanshKarani011235 <riyansh.karani.011235@gmail.com>
2017-05-04 16:46:17 +0530

    bugfixes in stitch

--- cloud/api/Stitch.js
+++ cloud/api/Stitch.js
@@ -187,11 +187,6 @@ var binaryStitch = (fileUrls) => {
             ffmpeg()
                 .input(fileUrls[0])
                 .input(fileUrls[1])
-                .videoCodec('libx264')
-                .output(outputFile)
-                .addOption('-filter_complex', '[0:v:0] [0:a:0] [1:v:0] [1:a:0] concat=n=2:v=1:a=1 [v] [a]')
-                .addOption('-map', '[v]')
-                .addOption('-map', '[a]')
                 .on('stderr', function(stderrLine) {
                     console.log('Stderr output: ' + stderrLine);
                 })
